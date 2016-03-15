63bec0e00796185a6f27682f7969e2834343ce6e
root <root@localhost.(none)>
2016-03-15 17:49:15 -0400

    Rename folder

--- Datto_example/letterCount.py
+++ Datto_example/letterCount.py
@@ -1,8 +1,8 @@
 def count(arg):
 	dict1={}
-	for i in word:
-		print i + ' occurs '+str(word.count(i))
-		dict1.update({i:word.count(i)})
+	for i in arg:
+		print i + ' occurs '+str(arg.count(i))
+		dict1.update({i:arg.count(i)})
 	print '\n'
 	mostOccurences=max(dict1.values())
 	if dict1.values().count(mostOccurences)>1:
@@ -13,11 +13,11 @@ def count(arg):
 				indexOfOccurences.append(i)
 		
 		for i in indexOfOccurences:
-			print dict1.keys()[i] +' is the most frequesnt character with '+str(mostOccurences)+' occurrences.'
+			print dict1.keys()[i] +' is the most frequent character with '+str(mostOccurences)+' occurrences.'
 	else:
-		print dict1.keys()[dict1.values().index(mostOccurences)] +' is the most frequesnt character with '+str(mostOccurences)+' occurrences.'
+		print dict1.keys()[dict1.values().index(mostOccurences)] +' is the most frequent character with '+str(mostOccurences)+' occurrences.'
 
 	print '\n\n'+str(dict1)
 
 word=raw_input('Word? ')
-count(word)
+count(word.lower())
